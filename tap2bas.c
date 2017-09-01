7ef0aa42c9ca169cee24f7df55e3227b12c67da1
Martin Møller Skarbiniks Pedersen <martin@linux.com>
2017-09-01 19:43:08 +0200

    initial commit

---
+++ tap2bas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libspectrum.h>
+
+
+int main(int argc, char **argv) {
+	if (argc<2) {
+		fprintf(stderr,"Missing tape-file argument\n");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+
+
+}
